d543214eb392d482d2e3e7d091a25f37a5d80cc1
melodyfff <307208327@qq.com>
2019-08-07 14:15:57 +0800

    :tada: init project

---
+++ src/main/java/com/xinchen/netty/package-info.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * netty-example
+ *
+ * reference:
+ *
+ * https://github.com/netty/netty/wiki/User-guide-for-4.x
+ *
+ * https://github.com/netty/netty/tree/4.1/example
+ *
+ *
+ * @author xinchen
+ * @date 07/08/2019 14:14
+ * @version 1.0
+ */
+package com.xinchen.netty;
